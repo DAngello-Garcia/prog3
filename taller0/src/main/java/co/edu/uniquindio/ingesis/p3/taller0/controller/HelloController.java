5cd90bfdc50cbd881d48c73964c51d978d3d1d4b
D'Angello <dangelloa.garcian@uqvirtual.edu.co>
2023-02-13 20:51:45 -0500

    clases

---
+++ taller0/src/main/java/co/edu/uniquindio/ingesis/p3/taller0/controller/HelloController.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.ingesis.p3.taller0.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ taller0/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module co.edu.uniquindio.ingesis.p3.taller0 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.ingesis.p3.taller0 to javafx.fxml;
+    exports co.edu.uniquindio.ingesis.p3.taller0;
+    exports co.edu.uniquindio.ingesis.p3.taller0.controller;
+    opens co.edu.uniquindio.ingesis.p3.taller0.controller to javafx.fxml;
+}
